9d1b9f951b23e882106a36b771ba476cf81f6714
sang <sang.chen@outlook.com>
2024-08-07 11:39:29 +0800

    update(auto): auto commit
    RENAMED:
    	trainer/numcompare/.gitignore
    	trainer/numcompare/README.md
    	trainer/numcompare/json_datasets.py
    	trainer/numcompare/requirements.txt
    	trainer/numcompare/run.py
    	trainer/numcompare/train.py
    	trainer/numcompare/train_data.json
    	trainer/peft/peft_example.ipynb
    	trainer/peft/peft_example.py
    	trainer/peft/peft_inference.py
    	trainer/peft/peft_train.py
    	trainer/tokenizer.py
    	trainer/trainer.py
    	trainer/tuvlomba/.gitignore
    	trainer/tuvlomba/README.md
    	trainer/tuvlomba/json_datasets.py
    	trainer/tuvlomba/requirements.txt
    	trainer/tuvlomba/run.py
    	trainer/tuvlomba/train.py
    	trainer/tuvlomba/train_data.json

--- trainer/tokenizer.py
+++ trainer/tokenizer.py
@@ -1,13 +1,18 @@
 from transformers import AutoTokenizer
 
+
 def tokenize(model_name, sentences):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors="pt")
+    encoded_input = tokenizer(
+            sentences, padding=True, truncation=True, return_tensors="pt")
     print("Encoded Input: ", encoded_input)
 
+
 def main():
-    tokenize("google-bert/bert-base-uncased", "In a hole in the ground there lived a hobbit.")
-    tokenize("google-bert/bert-base-cased", "In a hole in the ground there lived a hobbit.")
+    tokenize("google-bert/bert-base-uncased",
+             "In a hole in the ground there lived a hobbit.")
+    tokenize("google-bert/bert-base-cased",
+             "In a hole in the ground there lived a hobbit.")
     tokenize("google-bert/bert-base-cased", [
             "But what about second breakfast?",
             "Don't think he knows about second breakfast, Pip.",
@@ -15,5 +20,6 @@ def main():
         ]
     )
 
-if __name__== "__main__" :
+
+if __name__ == "__main__":
     main()
